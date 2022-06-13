95b27e9e993ed66483b33e99b94e01f823b13926
PumaMargo <margaret.kyselgova@gmail.com>
2022-06-13 12:58:57 +0300

    add some model stuff

---
+++ fork-back/Models/Account.cs
@@ -0,0 +1,10 @@
+﻿namespace fork_back.Models
+{
+    public class Account
+    {
+        public int Id { get; set; }
+        public string Loging { get; set; } = string.Empty;
+        public string Name { get; set; } = string.Empty;
+        public string LastName { get; set; } = string.Empty;
+    }
+}
