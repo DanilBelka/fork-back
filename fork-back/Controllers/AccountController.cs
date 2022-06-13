5991e2093257470618e444307580c9b17c30eb85
DanilBelka <danil.belikov.y.gd@gmail.com>
2022-06-13 21:55:48 +0300

    add Account Controller

---
+++ fork-back/Controllers/AccountController.cs
@@ -0,0 +1,112 @@
+﻿using fork_back.DataContext;
+using fork_back.Models;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using System.ComponentModel.DataAnnotations;
+using System.Net.Mime;
+
+namespace fork_back.Controllers
+{
+    public class AccountsController : BaseController
+    {
+        DatabaseContext DataContext { get; init; }
+        ILogger<AccountsController> Logger { get; init; }
+
+        public AccountsController(DatabaseContext dbContext, ILogger<AccountsController> logger)
+        {
+            DataContext = dbContext;
+            Logger = logger;
+        }
+
+        [HttpGet("List")]
+        public async Task<IEnumerable<Account>> GetListAsync([Range(1, MaxPageCount)] int? limit,
+                                                             [Range(0, int.MaxValue)] int? offset)
+        {
+            var res = await DataContext.Accounts.Include(a => a.Tickets)
+                                                .Skip(offset ?? 0)
+                                                .Take(limit ?? MaxPageCount)
+                                                .ToListAsync();
+            return res;
+        }
+
+        [HttpGet("{id:int}")]
+        public async Task<ActionResult<Account>> GetAccountAsync([Range(0, int.MaxValue)] int id)
+        {
+            var res = await DataContext.Accounts.Include(a => a.Tickets)
+                                                .FirstOrDefaultAsync(a => a.Id == id);
+
+            if (res == default)
+            {
+                return NotFound();
+            }
+
+            return res;
+        }
+
+        [HttpDelete("{id:int}")]
+        public async Task<ActionResult> DeleteAccountAsync([Range(0, int.MaxValue)] int id)
+        {
+            var res = await DataContext.Accounts.FirstOrDefaultAsync(a => a.Id == id);
+            if (res == default)
+            {
+                return NotFound();
+            }
+
+            DataContext.Accounts.Remove(res);
+            await DataContext.SaveChangesAsync();
+
+            return Ok();
+        }
+
+        [HttpPost]
+        [Consumes(MediaTypeNames.Application.Json)]
+        public async Task<ActionResult<Account>> CreateAccountAsync(Account account)
+        {
+            var isLoginBusy = await DataContext.Accounts.AnyAsync(a => a.Login == account.Login);
+            if (isLoginBusy)
+            {
+                ModelState.AddModelError(nameof(Account.Login), "Login is already used.");
+                return ValidationProblem();
+            }
+
+            await DataContext.Accounts.AddAsync(account);
+            await DataContext.SaveChangesAsync();
+
+            return account;
+        }
+
+        [HttpPut]
+        [Consumes(MediaTypeNames.Application.Json)]
+        public async Task<ActionResult<Account>> EditAccountAsync(Account account)
+        {
+            var hasTickets = account.Tickets?.Any() ?? false;
+            if (hasTickets)
+            {
+                ModelState.AddModelError(nameof(Account.Tickets), "Use other API to update ticket list.");
+                return ValidationProblem();
+            }
+
+            var isLoginBusy = await DataContext.Accounts.AnyAsync(a => a.Login == account.Login &&
+                                                                       a.Id != account.Id);
+            if (isLoginBusy)
+            {
+                ModelState.AddModelError(nameof(Account.Login), "Login is already used by other account.");
+                return ValidationProblem();
+            }
+
+            var res = await DataContext.Accounts.FirstOrDefaultAsync(a => a.Id == account.Id);
+            if (res == default)
+            {
+                return NotFound();
+            }
+
+            res.FirstName = account.FirstName;
+            res.LastName = account.LastName;
+            //res.Tickets = account.Tickets;       // We do not update tickets list here
+
+            await DataContext.SaveChangesAsync();
+
+            return account;
+        }
+    }
+}
