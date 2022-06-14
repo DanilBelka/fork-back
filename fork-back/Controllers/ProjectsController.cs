afab5b38924f0f946db9384530e695720224947d
DanilBelka <danil.belikov.y.gd@gmail.com>
2022-06-14 08:51:03 +0300

    add Projects Controller

---
+++ fork-back/Controllers/ProjectsController.cs
@@ -0,0 +1,113 @@
+﻿using fork_back.DataContext;
+using fork_back.Models;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using System.ComponentModel.DataAnnotations;
+using System.Net.Mime;
+
+namespace fork_back.Controllers
+{
+    public class ProjectsController : BaseController
+    {
+        DatabaseContext DataContext { get; init; }
+        ILogger<ProjectsController> Logger { get; init; }
+
+        public ProjectsController(DatabaseContext dbContext, ILogger<ProjectsController> logger)
+        {
+            DataContext = dbContext;
+            Logger = logger;
+        }
+
+        [HttpGet("List")]
+        public async Task<IEnumerable<Project>> GetListAsync([Range(1, MaxPageCount)] int? limit,
+                                                           [Range(0, int.MaxValue)] int? offset)
+        {
+            var res = await DataContext.Projects.Include(p => p.Epics)
+                                                .Skip(offset ?? 0)
+                                                .Take(limit ?? MaxPageCount)
+                                                .ToListAsync();
+            return res;
+        }
+
+        [HttpGet("{id:int}")]
+        public async Task<ActionResult<Project>> GetProjectAsync([Range(1, int.MaxValue)] int id)
+        {
+            var res = await DataContext.Projects.Include(p => p.Epics)
+                                                .FirstOrDefaultAsync(p => p.Id == id);
+
+            if (res == default)
+            {
+                return NotFound();
+            }
+
+            return res;
+        }
+
+        [HttpDelete("{id:int}")]
+        public async Task<ActionResult> DeleteProjectAsync([Range(1, int.MaxValue)] int id)
+        {
+            var res = await DataContext.Projects.FirstOrDefaultAsync(p => p.Id == id);
+            if (res == default)
+            {
+                return NotFound();
+            }
+
+            DataContext.Projects.Remove(res);
+            await DataContext.SaveChangesAsync();
+
+            return Ok();
+        }
+
+        [HttpPost]
+        [Consumes(MediaTypeNames.Application.Json)]
+        public async Task<ActionResult<Project>> CreateProjectAsync(Project project)
+        {
+            var isUrlBusy = await DataContext.Projects.AnyAsync(p => p.Url == project.Url);
+            if (isUrlBusy)
+            {
+                ModelState.AddModelError(nameof(Project.Url), "Url is already used.");
+                return ValidationProblem();
+            }
+
+            await DataContext.Projects.AddAsync(project);
+            await DataContext.SaveChangesAsync();
+
+            return project;
+        }
+
+        [HttpPut]
+        [Consumes(MediaTypeNames.Application.Json)]
+        public async Task<ActionResult<Project>> EditProjectAsync(Project project)
+        {
+            var hasTickets = project.Epics?.Any() ?? false;
+            if (hasTickets)
+            {
+                ModelState.AddModelError(nameof(Project.Epics), "Use other API to update epics list.");
+                return ValidationProblem();
+            }
+
+            var isUrlBusy = await DataContext.Projects.AnyAsync(p => p.Url == project.Url &&
+                                                                       p.Id != project.Id);
+            if (isUrlBusy)
+            {
+                ModelState.AddModelError(nameof(Project.Url), "Url is already used by other project.");
+                return ValidationProblem();
+            }
+
+            var res = await DataContext.Projects.FirstOrDefaultAsync(p => p.Id == project.Id);
+            if (res == default)
+            {
+                return NotFound();
+            }
+
+            res.Name = project.Name;
+            res.Description = project.Description;
+            res.Url = project.Url;
+            //res.Epics = project.Epics;       // We do not update epics list here
+
+            await DataContext.SaveChangesAsync();
+
+            return project;
+        }
+    }
+}
